7635b035b5778717a9c14b959600df5f4a8c846c
Poucheong Chong <pochong@ucsc.edu>
2021-01-26 21:02:20 -0800

    created a sample navbar

---
+++ src/components/MainComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Components.css';
+
+function Main() {
+    return (
+        <div className="Main-header">
+            <div className="Main">
+                <h1>Hi this is a test</h1>
+            </div>
+        </div>
+    );
+}
+export default Main;
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Components.css';
+
+function Navbar() {
+    return (
+        <div className="Nav-header">
+            <ul className="Nav">
+                <li className = "navbar"><a>Home</a></li>
+                <li className = "navbar"><a>About</a></li>
+                <li className = "navbar"><a>Project</a></li>
+            </ul>
+        </div>
+    );
+}
+export default Navbar;
